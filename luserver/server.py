86d096114356f27af764be15a5a00bfed1499976
lcdr <lcdr@localhost>
2018-05-09 09:14:35 +0200

    v2018.05.09 - fixed a model building bug - added more tests

--- luserver/server.py
+++ luserver/server.py
@@ -3,16 +3,15 @@ import logging
 import os
 import struct
 import time
-from typing import cast, Iterable, Set, SupportsBytes, Union
+from typing import Callable, cast, Iterable, Set, SupportsBytes, Union
 
 import pyraknet.server
+from pyraknet.bitstream import c_ubyte, c_uint, c_ushort, ReadStream
 from pyraknet.messages import Address, Message
-from .messages import MSG_TO_ENUM, GameMessage, MessageType, WorldClientMsg, WorldServerMsg
+from pyraknet.server import Event
+from .messages import ENUM_TO_MSG, MSG_TO_ENUM, GameMessage, LUMessage, MessageType, WorldClientMsg, WorldServerMsg
 
 log = logging.getLogger(__name__)
-
-c_uint = struct.Struct("<I")
-c_ushort = struct.Struct("<H")
 
 class Server:
 	SERVER_PASSWORD = b"3.25 ND1"
@@ -21,9 +20,31 @@ class Server:
 		self._server = pyraknet.server.Server(address, max_connections, Server.SERVER_PASSWORD)
 		self.not_console_logged_packets: Set[str] = set()
 		self.file_logged_packets: Set[str] = set()
+		self._packet_handlers: Dict[Tuple[int, int], List[Callable[..., None]]] = {}
+		self._server.add_handler(Event.UserPacket, self._on_lu_packet)
 
 	def _on_lu_packet(self, data: bytes, address: Address) -> None:
 		self._log_packet(data, received=True)
+		stream = ReadStream(data, unlocked=True)
+		stream.skip_read(1)
+		header = stream.read(c_ushort)
+		subheader = stream.read(c_ubyte)
+		stream.skip_read(4)
+		if header == MessageType.WorldServer.value and subheader == WorldServerMsg.Routing:
+			stream.skip_read(4)
+			header = stream.read(c_ushort)
+			subheader = stream.read(c_ubyte)
+			stream.skip_read(4)
+		read_offset = stream.read_offset
+		if (header, subheader) in self._packet_handlers:
+			for handler in self._packet_handlers[(header, subheader)]:
+				stream.read_offset = read_offset
+				handler(stream, address)
+
+	def register_handler(self, packet_id: LUMessage, handler: Callable[[ReadStream, Address], None]) -> None:
+		header = ENUM_TO_MSG[type(packet_id)]
+		subheader = packet_id
+		self._packet_handlers.setdefault((header, subheader), []).append(handler)
 
 	def _packetname(self, data: bytes) -> str:
 		from .modules.mail import MailID
@@ -33,14 +54,14 @@ class Server:
 			header = data[13]
 			subheader = data[15]
 		if (header, subheader) == (MessageType.WorldServer.value, WorldServerMsg.GameMessage) or (header, subheader) == (MessageType.WorldClient.value, WorldClientMsg.GameMessage):
-			message_id = cast(int, c_ushort.unpack(data[16:18])[0])
+			message_id = cast(int, c_ushort._struct.unpack(data[16:18])[0])
 			try:
 				message_name = GameMessage(message_id).name
 			except ValueError:
 				message_name = str(message_id)
 			return "GameMessage/" + message_name
 		if (header, subheader) == (MessageType.WorldServer.value, WorldServerMsg.Mail) or (header, subheader) == (MessageType.WorldClient.value, WorldClientMsg.Mail):
-			mail_id = cast(int, c_uint.unpack(data[8:12])[0])
+			mail_id = cast(int, c_uint._struct.unpack(data[8:12])[0])
 			try:
 				packetname = MailID(mail_id).name
 			except ValueError:
